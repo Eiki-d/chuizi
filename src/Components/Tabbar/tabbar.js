c6b282d72223ad90c558253e881829eadb60e309
Amy-d123 <2958376579@qq.com>
2020-01-09 10:09:35 +0800

    A-2

--- src/Components/Tabbar/tabbar.js
+++ src/Components/Tabbar/tabbar.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react'
+import {
+  NavLink
+} from 'react-router-dom'
 import "./tabbar.scss"
 class Tabbar extends Component{
     render(){
         return <div className="tabbar">
             <ul className="tabbar_list">
                 <li>
+                    <NavLink to="/home" activeClassName="active">
                     <span className="iconfont icon-shouye"></span>
                     <span className="tabbar_title">首页</span>
+                    </NavLink>
                 </li>
                 <li>
+                    <NavLink to="/classify" activeClassName="active">
                     <span className="iconfont icon-fenlei"></span>
                     <span className="tabbar_title">分类</span>
+                    </NavLink>
                 </li>
                 <li>
+                    <NavLink to="/cart" activeClassName="active">
                     <span className="iconfont icon-cartproductStyle"></span>
                     <span className="tabbar_title">购物车</span>
+                    </NavLink>
                 </li>
                 <li>
-                    <span className="iconfont icon-tabmeinactive
+                    <NavLink to="/center" activeClassName="active">
+                    <span className="iconfont icon-gerenzhongxinwode-xianxing
 "></span>
                     <span className="tabbar_title">个人中心</span>
+                    </NavLink>
                 </li>
                 
             </ul>
