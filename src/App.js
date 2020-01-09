717ed58412c08142e79c5d697876a66d39b8aec2
Amy-d123 <2958376579@qq.com>
2020-01-09 10:18:55 +0800

    A-3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Navbar from './Components/Navbar/navbar'
 import Tabbar from './Components/Tabbar/tabbar';
 
 class App extends Component {
   render(){
     return <div>
-        <Navbar/>
         {this.props.children}
         <Tabbar/>
       </div>
--- src/Views/Home/home.js
+++ src/Views/Home/home.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import "./home.scss"
+import Navbar from '../../Components/Navbar/navbar'
 
 class Home extends Component {
     render(){
-        return <div>Home</div>
+        return <div id="Home">
+            
+            <header className="iconfont icon-chuizi"></header>
+            <Navbar/>
+        </div>
     }
 }
 
